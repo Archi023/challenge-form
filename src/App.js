e8db43c27ad580a0abeebc408ea98e220c4661e9
Archi023 <archipatel65161@gmail.com>
2023-07-04 00:11:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const App = () => {
+  
+ const[name,setname]=useState();
+ const[fname,setfname]=useState();
+
+  const inputEvent =(event)=>{
+        setname(event.target.value);
+  }
+
+ const onsubmit = (event) =>{
+       event.preventDefault();
+       setfname(name);
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={onsubmit}>
+        <div className="container col-3 border rounded my-5">
+          <center>
+            <h2>Hello{fname}</h2>
+          </center>
+          <div className="form-group my-4">
+            <label>User Name</label>
+            <input
+              type="text"
+              className="form-control my-2"
+              placeholder="Enter your name"
+              onChange={inputEvent}
+              value={name}
+            />
+        
+          </div>
+
+          <div className="form-group my-4">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control my-2"
+              placeholder="Password"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="btn btn-primary my-2 mb-3 contanier col-12"
+            
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
   );
-}
+};
 
 export default App;
